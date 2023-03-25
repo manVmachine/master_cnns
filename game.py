2c460ad7273e679c113db7e8ee863b0b0c6a6402
manVmachine <95592373+manVmachine@users.noreply.github.com>
2023-03-25 14:43:49 -0500

    Create game.py

---
+++ game.py
@@ -0,0 +1,125 @@
+import pygame
+import random 
+import numpy as np
+
+
+buffer = 10
+screen_width, screen_height = 1000, 600
+
+config = {
+
+    "large": {
+        "number": 7,
+        "location": 115,
+        "scale": 100,
+        "points": 10
+        },
+    "medium": {
+        "number": 8,
+        "location": 170,
+        "scale": 50,
+        "points": 25
+        },
+    "small": {
+        "number": 5,
+        "location": 215,
+        "scale": 25,
+        "points": 30
+        }
+
+}
+
+class Target(pygame.sprite.Sprite):
+    def __init__(self, image, x, y, points):
+        super().__init__()
+        self.image = image
+        self.points = points
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+    
+    def get_score(self):
+        return self.points
+    
+def generate_targets(screen, group):
+    xs, ys = get_offsets(screen, group, buffer)
+    return pygame.sprite.Group(
+        [Target(load_image('target.png', group['scale']), xs[i], ys[i], group['points']) for i in range(group['number'])]
+    )
+
+def ranks(sample):
+    indices = sorted(range(len(sample)), key=lambda i: sample[i])
+    return sorted(indices, key=lambda i: indices[i])
+
+def sample(n=40, k=4, d=10):
+    sample = random.sample(range(n-(k-1)*(d-1)), k)
+    return np.array([s + (d-1)*r for s, r in zip(sample, ranks(sample))])
+
+def get_offsets(screen, group, buffer=10):
+    return (sample(screen.get_width() - group['scale'] - buffer, group['number'], group['scale']) + buffer, 
+            np.full(group['number'], screen.get_height() - group['location'] - buffer))
+
+def load_image(path, scale):
+    return pygame.transform.scale(pygame.image.load(path).convert_alpha(), (scale, scale))
+
+def generate_groups(screen, config):
+    return [generate_targets(screen, params) for group, params in config.items()]
+
+def check_hit(target_group, position, score):
+    for target in target_group:
+        if target.rect.collidepoint(position[0], position[1]):
+            score += target.get_score()
+            target_group.remove(target)
+    return score
+
+def get_count(targets):
+    count = 0
+    for group in targets:
+        count += len(group.sprites())
+    return count
+    
+# Initialize Pygame
+pygame.init()
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+background_image = pygame.image.load("background.jpg").convert()
+
+crosshair_image = pygame.transform.scale(pygame.image.load('crosshair3.png'), (200, 200))
+crosshair_rect = crosshair_image.get_rect()
+
+pygame.mouse.set_visible(False)
+
+
+score = 0
+targets = generate_groups(screen, config)
+
+running = True
+while running:
+    # check for user events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                for group in targets:
+                    score = check_hit(group, event.pos, score)
+
+    screen.blit(background_image, (0, 0))
+    for group in targets:
+        group.draw(screen)
+
+    crosshair_rect.center = pygame.mouse.get_pos()
+    screen.blit(crosshair_image, crosshair_rect)
+    screen.blit(pygame.font.Font(None, 36).render("Score: " + str(score), True, (255, 255, 255)), (10, 10))
+
+    if get_count(targets) == 0:
+        screen.blit(pygame.font.Font(None, 36).render("YOU WIN!", True, (255, 255, 255)), (150, 150))
+
+
+    pygame.display.update()
+    clock.tick(60)
+
+# Quit Pygame
+pygame.quit()
+
